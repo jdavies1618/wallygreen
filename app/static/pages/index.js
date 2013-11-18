7e9c7fed90907f34edc7a25b5883999d35660ca9
John Davies <john@knewton.com>
2013-11-18 15:06:30 -0500

    Add some form validation
    Missing validation that players are actually selected, b/c dammit.

--- app/static/pages/index.js
+++ app/static/pages/index.js
@@ -37,15 +37,9 @@
 		var max = $(this).val();
 		var curMax = $("#first_player_score_id option:last").val();
 		if (curMax > max) {
-			if (!(curMax == 21 && max == 11)) {
-				alert("Max Score Madness?! cur="+curMax+"; max="+max);
-			}
 			$("#first_player_score_id option:gt("+max+")").remove();
 			$("#second_player_score_id option:gt("+max+")").remove();
 		} else if (max > curMax) {
-			if (!(curMax == 11 && max == 21)) {
-				alert("Max Score Madness?! cur="+curMax+"; max="+max);
-			}
 			for(var i=Number(curMax)+1;i<=max;i++){
 				$("#first_player_score_id").append(
 					"<option value='"+i+"'>"+i+"</option>");
@@ -54,6 +48,21 @@
 			}
 		}
 	});
+
+	$(document).ready(function() {
+		jQuery.validator.addMethod("player_id", function( value, element ) {
+			var result = this.optional(element) || $("#second_player_id").val() !== $("#first_player_id").val();
+			return result;
+		}, "Players must have different ids.");
+		jQuery.validator.addMethod("scores", function (value, element ) {
+			var result = this.optional(element) || (
+							$("#first_player_score_id").val() === $("#maxScore").val() ||
+						 	$("#second_player_score_id").val() === $("#maxScore").val());
+			return result;
+		}, "One player must have the max score.");
+		$("#score-form").validate({});
+	});
+
 
 	//------------------------------
 	//
